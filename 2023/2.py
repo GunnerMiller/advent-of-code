9da649d319a7303aa55e86553d8619b23ec945fc
Gunner Miller <gunner.miller@outlook.com>
2023-12-01 23:31:45 -0600

    part 2

--- 2023/2.py
+++ 2023/2.py
@@ -11,8 +11,10 @@ for gameIndex, game in enumerate(inputs):
     game = game[game.index(":")+2:]
     badgame = 0
     sets = game.split(";")
+    maxRedCount = 0
+    maxGreenCount = 0
+    maxBlueCount = 0
     for set in sets:
-        # print(set)
         currentRedCount = 0
         currentGreenCount = 0
         currentBlueCount = 0
@@ -21,10 +23,22 @@ for gameIndex, game in enumerate(inputs):
             x = pull.strip().split(" ")
             if x[1] == "red":
                 currentRedCount += int(x[0])
-            if x[1] == "green":
+                if maxRedCount == 0:
+                    maxRedCount = int(x[0])
+                elif int(x[0]) > maxRedCount:
+                    maxRedCount = int(x[0])
+            elif x[1] == "green":
                 currentGreenCount += int(x[0])
-            if x[1] == "blue":
+                if maxGreenCount == 0:
+                    maxGreenCount = int(x[0])
+                elif int(x[0]) > maxGreenCount:
+                    maxGreenCount = int(x[0])
+            elif x[1] == "blue":
                 currentBlueCount += int(x[0])
+                if maxBlueCount == 0:
+                    maxBlueCount = int(x[0])
+                elif int(x[0]) > maxBlueCount:
+                    maxBlueCount = int(x[0])
         if currentRedCount > redCount:
             badgame = 1
         if currentGreenCount > greenCount:
@@ -33,6 +47,8 @@ for gameIndex, game in enumerate(inputs):
             badgame = 1
     if badgame == 0:
         out1 += gameIndex + 1
+    power = maxRedCount * maxGreenCount * maxBlueCount
+    out2 += (power)
         
 
     
