e4332edc5106417204faca1081870946c74d19e8
Gunner Miller <gunner.miller@outlook.com>
2023-12-05 00:48:23 -0600

    I AM A GOD HOLYYYYYYY

--- 2023/5.py
+++ 2023/5.py
@@ -2,8 +2,8 @@ from collections import defaultdict
 
 out1 = 0
 out2 = 0
-# inputs = open("5.txt", "r").readlines()
-inputs = open("5.sample.txt", "r").readlines()
+inputs = open("5.txt", "r").readlines()
+# inputs = open("5.sample.txt", "r").readlines()
 
 seeds = []
 maps = {}
@@ -19,14 +19,8 @@ while i < len(inputs):
             key = line.split()[0].split("-")[2]
             map = defaultdict(str)
             while i+1 < len(inputs) and inputs[i+1][0].isdigit():
-                nums = inputs[i+1]
-                dest, source, length = nums.split()
-                source = int(source)
-                dest = int(dest)
-
-
-                for x in range(int(length)):
-                    map[str(source+x)] = str(dest+x)
+                dest, source, range_length = inputs[i+1].split()
+                map[source] = [dest, range_length]
                 i+=1
 
             maps[key] = map
@@ -40,21 +34,21 @@ for seedValue in seeds:
 
     # drill to location
     while next_map != "":
-        temp = maps[next_map][value]
-        value = temp if temp != "" else value
+        map = maps[next_map]
+        for source in map.keys():
+            dest, range_length = map[source]
+            source = int(source)
+            value = int(value)
+            if value >= source and value <= source + int(range_length) - 1:
+                value = value + int(dest) - int(source)
+                break
+        
         next_map = mappings[next_map]
-
-
 
     if curr_low == None:
         curr_low = value
     else:
         curr_low = min(curr_low, value)
-
-    
-
-        
-
 
 print(curr_low)
 print(out2)
