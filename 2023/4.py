64259aa5c5b87c9d41eab5f5354653bc5b98cbf9
Gunner Miller <gunner.miller@outlook.com>
2023-12-05 22:54:12 -0600

    part 2 B-)

--- 2023/4.py
+++ 2023/4.py
@@ -1,6 +1,8 @@
 from collections import defaultdict
+import datetime
 import math
 
+start = datetime.datetime.now()
 d =  {}
 cards = {}
 
@@ -8,6 +10,7 @@ cards = {}
 inputs = open("4.txt", "r").readlines()
 
 out1 = 0
+out2 = 0
 for i, card in enumerate(inputs, 1):
     card = card.split(':')[1].strip()	
     winners, nums = card.split("|")
@@ -29,4 +32,27 @@ for i, card in enumerate(inputs, 1):
         out1 += (1 * math.pow(2, matches - 1))
     cards[i] = matches
 
+stack = []
+for i in range(1, len(inputs)+1):
+    stack.append(i)
+
+
+
+while len(stack) > 0:
+    out2 += 1
+    curr_card = stack.pop()
+    num_matches = cards[curr_card]
+    if num_matches > 0:
+        # print ("curr_card=" + str(curr_card) + " num_matches=" + str(num_matches))
+        for i in range(1, 1 + num_matches):
+            stack.append(curr_card + i)
+        
+    
+
+end = datetime.datetime.now()
+result = end-start
+
 print(int(out1))
+print(int(out2))
+print("TIME (sec)= " + str(result.total_seconds()))
+print("TIME (ms)= " + str(int(result.total_seconds())* 1000))
