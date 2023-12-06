5c97adf3f472ee03664fc89482895b32d8934d1a
Gunner Miller <gunner.miller@outlook.com>
2023-12-05 23:25:44 -0600

    part two the same thing???

--- 2023/6.py
+++ 2023/6.py
@@ -1,23 +1,24 @@
+import sys
 import math
 p1 = 0
 
-input = open("7.txt", "r").readlines()
-# input = open("7.sample.txt", "r").readlines()
+input = open("6.txt", "r").readlines()
+# input = open("6.sample.txt", "r").readlines()
 
-times = input[0].split(":")[1].strip().split()
-dists = input[1].split(":")[1].strip().split()
-Beats = []
-for i in range(len(times)):
-    time = int(times[i])
-    dist = int(dists[i])
-    beats = 0
-    traveled = 0
-    for j in range(1,int(time)):
-        acc = j
-        traveled = acc * (time - j)
-        if traveled > dist:
-            beats += 1
-    Beats.append(beats)
+times = input[0].split(":")[1].replace(" ", "").strip()
+dists = input[1].split(":")[1].replace(" ", "").strip()
+print(times)
+print(dists)
 
-p1 += math.prod(Beats)
+time = int(times)
+dist = int(dists)
+beats = 0
+traveled = 0
+for j in range(1,int(time)):
+    acc = j
+    traveled = acc * (time - j)
+    if traveled > dist:
+        beats += 1
+
+p1 += beats
 print(p1)
