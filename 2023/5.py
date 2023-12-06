e12de80a30d60f6a3f625a54e36621554d819038
Gunner Miller <gunner.miller@outlook.com>
2023-12-05 20:51:09 -0600

    p2 zoomin

--- 2023/5.py
+++ 2023/5.py
@@ -1,61 +1,55 @@
-from collections import defaultdict
-
-out1 = 0
-out2 = 0
 inputs = open("5.txt", "r").readlines()
-# inputs = open("5.sample.txt", "r").readlines()
-
-seeds = []
-maps = {}
-mappings = defaultdict(str, {"seed":"soil","soil":"fertilizer","fertilizer":"water","water":"light","light":"temperature","temperature":"humidity", "humidity":"location"})
-
-i = 0
-while i < len(inputs):
-    line = inputs[i]
-    if i == 0:
-        seeds = line.split(":")[1].strip().split()
-    elif line != "\n": 
-        if not line[0].isdigit():
-            key = line.split()[0].split("-")[2]
-            map = defaultdict(str)
-            while i+1 < len(inputs) and inputs[i+1][0].isdigit():
-                dest, source, range_length = inputs[i+1].split()
-                map[source] = [dest, range_length]
-                i+=1
-
-            maps[key] = map
-    i += 1
-
-curr_low = None
-x = 0
-while x < len(seeds):
-    seedsRange = seeds[x+1]
-    initialValue = seeds[x]
-    for ind in range(int(seedsRange)):
-        value = int(initialValue) + ind
-        # print(value)
-        key = "seed"
-        next_map = mappings[key]
-
-    # drill to location
-        while next_map != "":
-            map = maps[next_map]
-            for source in map.keys():
-                dest, range_length = map[source]
-                source = int(source)
-                value = int(value)
-                if value >= source and value <= source + int(range_length) - 1:
-                    value = value + int(dest) - int(source)
-                    break
-            
-            next_map = mappings[next_map]
-
-        if curr_low == None:
-            curr_low = value
-        else:
-            curr_low = min(curr_low, value)
-    
-    x+=2
-
-print(curr_low)
-print(out2)
+import datetime
+start = datetime.datetime.now()
+curr_lowest = None
+seeds = inputs[0].split(":")[1].strip().split()
+for x in range(0, len(seeds), 2):
+    i = 1
+    values = {int(seeds[x]) : int(seeds[x]) + int(seeds[x+1]) - 1}
+    while i < len(inputs):
+        line = inputs[i]
+        if line != "\n": 
+            if not line[0].isdigit():
+                transformed = {}
+                while i+1 < len(inputs) and inputs[i+1][0].isdigit():
+                    dest, source, range_length = inputs[i+1].split()
+                    ruleStart = int(source)
+                    ruleEnd = ruleStart + int(range_length) - 1
+                    t_factor = int(dest) - int(source)
+                    
+                    stack = list(values.keys())
+                    while len(stack) > 0:
+                        key = stack.pop()
+                        valueStart = int(key)
+                        valueEnd = int(values[key])
+                        if valueStart >= ruleStart and valueEnd <= ruleEnd:
+                            del values[key]
+                            transformed[valueStart + t_factor] = valueEnd + t_factor
+                        elif ruleStart >= valueStart and ruleEnd <= valueEnd:
+                            del values[key]
+                            transformed[ruleStart + t_factor] = ruleEnd + t_factor
+                            values[valueStart] = ruleStart - 1
+                            stack.append(valueStart)
+                            values[ruleEnd + 1] = valueEnd
+                            stack.append(ruleEnd + 1)
+                        elif ruleStart >= valueStart and ruleStart <= valueEnd:
+                            del values[key]
+                            transformed[ruleStart + t_factor] = valueEnd = t_factor
+                            values[valueStart] = ruleStart - 1
+                            stack.append(valueStart)
+                        elif ruleEnd >= valueStart and ruleEnd <= valueEnd:
+                            del values[key] 
+                            transformed[valueStart + t_factor] = ruleEnd + t_factor
+                            values[ruleEnd + 1] = valueEnd
+                            stack.append(ruleEnd + 1)
+                    i+=1
+                for ele in transformed.keys():
+                    values[ele] = transformed[ele]
+        i+=1
+    temp = list(values.keys())
+    temp.sort()
+    curr_low = temp[0]
+    curr_lowest = curr_low if curr_lowest == None else min(curr_lowest, curr_low)
+end = datetime.datetime.now()
+print(curr_lowest)
+print("RUNTIME=" + str(end-start))
