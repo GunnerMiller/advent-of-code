e627992f8919c2dad4c412405e0b9bfc2eba2c4d
Gunner Miller <gunner.miller@outlook.com>
2023-12-12 20:25:43 -0600

    housekeeping

---
+++ 2023/11.py
@@ -0,0 +1,4 @@
+input = open("11.txt", "r").readlines()
+input = open("11.sample.txt", "r").readlines()
+p1 = 0
+p2 = 0
---
+++ 2023/14.py
@@ -0,0 +1,4 @@
+input = open("14.txt", "r").readlines()
+input = open("14.sample.txt", "r").readlines()
+p1 = 0
+p2 = 0
