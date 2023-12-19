c35c528f933ea56361a4d4444e32658c8d9b6405
Gunner Miller <gunner.miller@outlook.com>
2023-12-18 22:01:28 -0600

    tmep

--- 2023/11.py
+++ 2023/11.py
@@ -1,13 +1,43 @@
-input = open("11.txt", "r").readlines()
+# input = open("11.txt", "r").readlines()
 input = open("11.sample.txt", "r").readlines()
 p1 = 0
 p2 = 0
 
-# make grid
-
-# find empty lines
-# expand input by those empty lines
+rows = []
+for line in input:
+    rows.append([*line.strip()])
+empty_rows = []
+for i, row in enumerate(rows):
+    empty = True
+    for char in row:
+        if char != '.':
+            empty = False
+            break
+    if empty:
+        empty_rows.append(i)
+for empty_row in reversed(empty_rows):
+    rows.insert(empty_row, [*'.' * len(rows[0])])
+empty_cols = []
+for col in range(len(rows[0])):
+    empty = True
+    for row in range(len(rows)):
+        char = rows[row][col]
+        if char != '.':
+            empty = False
+            break
+    if empty:
+        empty_cols.append(col)
+for ec in reversed(empty_cols):
+    for row in rows:
+        row.insert(ec, '.')
 
 # find coordinates of all hashes
+galaxies = []
+#  for y:
+#   for x:
+#     galaxies.append(x,y)
+
 # determine pairs
 # find shortest path by |x1-x2| + |y2-y1|
+
+# sum shortest paths
--- 2023/12.py
+++ 2023/12.py
@@ -1,4 +1,4 @@
-input = open("12.txt", "r").readlines()
+# input = open("12.txt", "r").readlines()
 input = open("12.sample.txt", "r").readlines()
 p1 = 0
 p2 = 0
