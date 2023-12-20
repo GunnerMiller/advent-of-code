20d6eabc5dd9e464c70b4ad1bd603cb6345c7421
Gunner Miller <gunner.miller@outlook.com>
2023-12-20 00:22:38 -0600

    temp

--- 2023/12.py
+++ 2023/12.py
@@ -6,8 +6,32 @@ p2 = 0
 # read line, split with spacec
 for line in input:
     line, rule = line.split()
-    print(line)
-    print(rule)
+    line = [*line]
+    
+    perms = [line]
+    while len(perms) > 0:
+        curr = perms.pop()
+        p = line.copy()
+        p[i] = '.'
+        q = line.copy()
+        q[i] = '#'
+        perms.append(p)
+        perms.append(q)
+
+    for perm in perms:
+        print(perm)
 # resolve wild cards, finding all permutations of the input line
-# find how many lines satify the rules given in split[1] from L6
-# p1 + sum of lines that satisfy
+    
+
+    # find how many lines satify the rules given in split[1] from L6
+    
+    # use this to get the groups of hashes
+    # hashes = ' '.join(line.split(".")).split()
+
+    # groups = [rule.split(",")]
+    # for group in groups
+    # pop groups
+    # try to find match through hashes
+    # end loop
+    # if groups not empty all rules
+    # p1 + sum of lines that satisfy
