d295ee634ec3f48adf770764671950a2b126a70f
Gunner Miller <gunner.miller@outlook.com>
2023-12-19 22:32:37 -0600

    txt files

--- 2023/12.py
+++ 2023/12.py
@@ -4,6 +4,10 @@ p1 = 0
 p2 = 0
 
 # read line, split with spacec
+for line in input:
+    line, rule = line.split()
+    print(line)
+    print(rule)
 # resolve wild cards, finding all permutations of the input line
 # find how many lines satify the rules given in split[1] from L6
 # p1 + sum of lines that satisfy
