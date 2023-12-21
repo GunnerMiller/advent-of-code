d9d75220ddf9f84387f4efb86f3063ad5c8bbe0f
GunnerMiller <120051714+GunnerMiller@users.noreply.github.com>
2023-12-21 01:01:01 +0000

    temp

--- 2023/13.py
+++ 2023/13.py
@@ -1,23 +1,44 @@
-import numpy as np
 # input = open("13.txt", "r").readlines()
 input = open("13.sample.txt", "r").readlines()
 p1 = 0
 p2 = 0
 
-matched = False
-cols = []
-rows = []
-
-grid = []
-curr = input[0]
+grids = [[]]
+gi = 0
 for line in input:
-    grid.append(line)
-# maybe optimize to check rows first?
-
-# Go by columns and seek match
-# numpy is good for finding hashes
-# get sum of indeces where hashes are
-# save sum to array of length columns
-# each index of array is sum at that index
-# use array for lookup of previously summed columns
-# repeat for rows if columns don't match/mirror
+    line = line.strip()
+    if len(line) > 0:
+        grids[gi].append(line)
+    else:
+        gi += 1
+        grids.append([])
+    
+for grid_index, grid in enumerate(grids):
+    if (grid_index + 1) % 2 != 0:
+        cols = {}
+        for c in range(len(grid[0])): 
+            l = []
+            for r in range(len(grid)):
+                l.append(grid[r][c])
+            l = ''.join(l)
+            print(l)
+            try:
+                j = cols[l]
+                if j == c-1:
+                    p1 += (c+1)*100
+                    print(p1)
+                    break
+            except:
+                cols[l] = c
+    else:
+        rows = {}   
+        for i, line in enumerate(grid):
+            print(line)
+            try:
+                j = rows[line]
+                if j == i-1:
+                    p1 += i + 1 
+                    print(p1)
+                    break
+            except:
+                rows[line] = i
