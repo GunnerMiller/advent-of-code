f666e703a34259c2f671e902c400b062d89c04ce
Gunner Miller <gunner.miller@outlook.com>
2023-12-22 13:11:48 -0600

    rip bozo

--- 2023/13.py
+++ 2023/13.py
@@ -1,5 +1,5 @@
-# input = open("13.txt", "r").readlines()
-input = open("13.sample.txt", "r").readlines()
+input = open("13.txt", "r").readlines()
+# input = open("13.sample.txt", "r").readlines()
 p1 = 0
 p2 = 0
 
@@ -12,10 +12,11 @@ for line in input:
     else:
         gi += 1
         grids.append([])
-    
+
 for grid_index, grid in enumerate(grids):
     if (grid_index + 1) % 2 != 0:
         cols = {}
+        c_mirrors = []
         for c in range(len(grid[0])): 
             l = []
             for r in range(len(grid)):
@@ -25,20 +26,22 @@ for grid_index, grid in enumerate(grids):
             try:
                 j = cols[l]
                 if j == c-1:
-                    p1 += (c+1)*100
-                    print(p1)
-                    break
+                    c_mirrors.append([j,c])
+                    p1+=c
             except:
                 cols[l] = c
+        print(c_mirrors)
     else:
         rows = {}   
+        r_mirrors = []
         for i, line in enumerate(grid):
             print(line)
             try:
                 j = rows[line]
                 if j == i-1:
-                    p1 += i + 1 
-                    print(p1)
-                    break
+                    r_mirrors.append([j,i])
+                    p1+=i*100
             except:
                 rows[line] = i
+        print(r_mirrors)
+print(p1)
