abea6154fa554d39cdb92f258bfbfa381f8645c7
GunnerMiller <120051714+GunnerMiller@users.noreply.github.com>
2023-12-22 23:44:36 +0000

    part one

--- 2023/14.py
+++ 2023/14.py
@@ -1,4 +1,32 @@
 input = open("14.txt", "r").readlines()
-input = open("14.sample.txt", "r").readlines()
-p1 = 0
-p2 = 0
+# input = open("../14.sample.txt", "r").readlines()
+out = 0
+
+def find_slot(line, index):
+    copy = line
+    copy = copy[:index]
+    # print(copy)
+    for x, char in enumerate(reversed(copy)):
+        if char == '#' or char == 'O':
+            return index - x
+    return index - len(copy)
+
+def swap(line, a, b):
+    if a != b:
+        temp = line[a]
+        line[a] = line[b]
+        line[b] = temp
+
+         
+rotato = zip(*input)
+grid = []
+for line in rotato:
+    line = list(line)
+
+    for i, char in enumerate(line):
+        if char == 'O':
+            swap(line, find_slot(line,i), i)
+    for i, char in enumerate(line):
+        if char == 'O':
+            out += (len(line) - i)
+print(out)
