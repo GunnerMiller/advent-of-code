ff92870296d4b39e5031ab58f752f575cf5a68f3
Gunner Miller <gunner.miller@outlook.com>
2023-12-22 22:31:03 -0600

    part two

--- 2023/15.py
+++ 2023/15.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
-# filename = '15.txt'
-filename = '15.sample.txt'
+filename = '15.txt'
+# filename = '15.sample.txt'
 try:
     input = open(filename, "r").read().replace("\n",'')
 except:
@@ -16,31 +16,37 @@ for line in input.split(','):
 p2 = 0
 map = defaultdict(list)
 for step, line in enumerate(input.split(',')):
-    key = line[0:2]
-    command = line[2:]
+    add = True
+    if line[-1] == '-':
+        key = line[:-1]
+        add = False
+    else:
+        key, focus = line.split('=')
     bucket = 0
     for char in key:
         bucket = (bucket + ord(char)) * 17 % 256
-    if command[0] == '=':
-        focus = command[-1]
-        if key not in (i[0] for i in map[bucket]):
+    in_bucket = [i[0] for i in map[bucket]]
+    if add:
+        if key not in (in_bucket):
             map[bucket].append([key, focus])
         else:
-            i = map[bucket].index(key)
+            i = in_bucket.index(key)
             map[bucket][i][1] = focus
     else:
         try:
-            map[bucket].remove(key)
-        except:
+            i = in_bucket.index(key)
+            del map[bucket][i]
+        except Exception as e:
             pass
     
 for i, bucket in enumerate(map.keys()):
     if len(map[bucket]) == 0:
         continue
     for i, ele in enumerate(map[bucket]):
-        box = bucket + 1
+        box = int(bucket) + 1
         slot = i + 1
-        focus = ele[1]
+        focus = int(ele[1])
         value = box * slot * focus
         print(value)
+        p2 += value
 print(p2)
